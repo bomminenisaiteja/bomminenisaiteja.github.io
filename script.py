6cf16676aeb08beaa7b94b06202848f373100f9f
bomminenisaiteja <63557130+bomminenisaiteja@users.noreply.github.com>
2020-04-19 11:08:29 +0530

    commit

--- script.py
+++ script.py
@@ -48,9 +48,9 @@ df['STATE/UT'] = df['STATE/UT'].astype('str')
 x = df["STATE/UT"]
 
 fig8 = go.Figure()
-fig8.add_trace(go.Bar(x=x, y=df["ACTIVE"],text=df["CONFIRMED"],textposition='inside',name="ACTIVE"))
+fig8.add_trace(go.Bar(x=x, y=df["ACTIVE"],name="ACTIVE"))
 fig8.add_trace(go.Bar(x=x, y=df["RECOVERED"],name="RECOVERED"))
-fig8.add_trace(go.Bar(x=x, y=df["DECEASED"],name="DECEASED"))
+fig8.add_trace(go.Bar(x=x, y=df["DECEASED"],text=df["CONFIRMED"],textposition='outside',name="DECEASED"))
 fig8.update_layout(barmode='stack', title_text='COVID19 INDIA')
 fig4=px.pie(df,names="STATE/UT",values="CONFIRMED")
 html = df.to_html()
